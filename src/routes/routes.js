4a9c78293c3d81c3fe70dee74dd6bed2657cd778
salmanyagaka <salma@buupass.com>
2021-11-05 01:29:59 +0300

    add previous order tables

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,12 +3,14 @@ import * as React from 'react'
 import { Route, Switch,  BrowserRouter} from 'react-router-dom'
 import landingPage from '../components/landingPage'
 import loginPage from '../components/loginPage'
+import pastOrdersPage from '../components/pastOrdersPage'
 
 const Routes = () => (
     <BrowserRouter>
     <Switch>
       <Route exact path='/' component={landingPage} />
       <Route exact path='/login' component={loginPage} />
+      <Route exact path='/orders' component={pastOrdersPage} />
 
     </Switch>
   </BrowserRouter>
