21e4daea02b9325efb7bfd43ff6058a4087c3da4
salmanyagaka <salma@buupass.com>
2021-11-05 00:54:06 +0300

    add login page and assets

--- src/common/navbar.js
+++ src/common/navbar.js
@@ -22,7 +22,7 @@ const Navbar = () => {
         <div class="nav-links">
           <a href="" >Menu</a>
           <a href="" >Past Orders</a>
-          <a href="" >Sign In</a>
+          <a href="/login" >Sign In</a>
           <i class="fa fa-shopping-cart" ></i>
         </div>
       </div>
