b613055e0e085a7e787b87aae11b70536af65f4d
salmanyagaka <salma@buupass.com>
2021-11-04 22:18:57 +0300

    setup project structure

---
+++ src/routes/route.js
@@ -0,0 +1,13 @@
+ 
+import * as React from 'react'
+import { Route, Switch,  BrowserRouter} from 'react-router-dom'
+
+
+const Routes = () => (
+    <BrowserRouter>
+    <Switch>
+      {/* <Route exact path='/' component={  } /> */}
+    </Switch>
+  </BrowserRouter>
+)
+export default Routes
